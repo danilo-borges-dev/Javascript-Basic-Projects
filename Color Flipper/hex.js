527e0a8d241cf7890df6c9353e9faa0ef4398e57
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-07-14 13:56:26 -0300

    Compreendendo a fundo o exercicio

--- Color Flipper/hex.js
+++ Color Flipper/hex.js
@@ -1,3 +1,6 @@
+// Faz o estudo detalhado deste código e entender a lógica
+// Digitar na frente a explicação de cada comando e também a lógica
+
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 // #F15025
 const btn = document.getElementById("btn");
