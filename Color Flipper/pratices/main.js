8c934b9c36b6becced39927552b9d721a17cb24b
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-07-16 14:24:37 -0300

    Mais um Exercicio de revisao realizado com sucesso

--- Color Flipper/pratices/main.js
+++ Color Flipper/pratices/main.js
@@ -1,17 +1,17 @@
-const hexColor = [1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const hex = [1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
 btn.addEventListener("click", function(){
-    let hex = "#";
+    let colorHex = "#";
     for(let i = 0; i < 6; i++){
-        hex += hexColor[getHexadecimalColor()];
+        colorHex += hex[generatorColor()];
     }
-    color.textContent = hex; // Altera o valor de texto que será exibido no elemento color
-    document.body.style.backgroundColor = hex;  // Estamos acessando o documento a prorpriedade body, em brodry estamos acessando o estilo das cores de fundo e alterando
+    color.textContent = colorHex;
+    document.body.style.backgroundColor = colorHex;
 });
 
-function getHexadecimalColor(){
-    return Math.floor(Math.random() * hexColor.length);
+function generatorColor(){
+    return Math.floor(Math.random() * hex.length);
 }
