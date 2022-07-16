54a5d7d9e1be12fb60ec623dd199a0e23035be9b
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-07-16 14:17:24 -0300

    Revisao do Exercicio Color Flipper

---
+++ Color Flipper/pratices_02/main.js
@@ -0,0 +1,17 @@
+const hex = [1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function (){
+    let colorHex = "#";
+    for(let i = 0; i < 6; i++){        
+        colorHex += hex[generator()];
+    }
+    color.textContent = colorHex;
+    document.body.style.backgroundColor = colorHex;
+});
+
+function generator(){
+    return Math.floor(Math.random() * hex.length);
+}
